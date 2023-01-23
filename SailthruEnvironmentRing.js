d159c9d110268586ca8945d8ad8f94ad1d2b0e79
Colin-Whelan <38737123+Colin-Whelan@users.noreply.github.com>
2023-01-23 11:34:34 -0500

    already created files

---
+++ SailthruEnvironmentRing.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name        Sailthru Environment Ring
+// @namespace   Violentmonkey Scripts
+// @match       https://my.sailthru.com/*
+// @grant       none
+// @version     1.0
+// @author      Colin Whelan
+// @description Add colored ring to Sailthru pages to remind/warn the user what environtment they are in
+// @require https://code.jquery.com/jquery-3.6.0.min.js
+// ==/UserScript==
+
+// avoids conflict with the code editor using jquery
+var jq = $.noConflict();
+
+
+let environtment = jq("#my-navbar-components :contains('Indigo')").text()
+
+
+let wrapper = document.getElementById("wrapper");
+
+let styles = {
+  'DEV': '10px solid indigo',
+  'QA': '10px solid green',
+  'PROD': '10px solid red',
+}
+
+wrapper.style.boxSizing = 'border-box'
+
+if(environtment.includes("DEV")){
+  wrapper.style.border = styles.DEV
+  // console.log('DEV')
+}else if(environtment.includes("QA")){
+  wrapper.style.border = styles.QA
+  // console.log('QA')
+}else if(environtment.includes("Indigo") && !environtment.includes("DEV") && !environtment.includes("QA")){
+  wrapper.style.border = styles.PROD
+  // console.log('PROD')
+} else {
+  // console.log('UNKNOWN')
+}
