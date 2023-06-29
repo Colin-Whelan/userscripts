594c0f89aad9019ca178ddecdc46b59267fe5211
Colin-Whelan <38737123+Colin-Whelan@users.noreply.github.com>
2023-06-29 16:16:48 -0400

    Update autoSaveCampaigns.js

--- scripts/sites/sailthru/autoSaveCampaigns.js
+++ scripts/sites/sailthru/autoSaveCampaigns.js
@@ -23,7 +23,6 @@ function convertToMilliseconds(timeStr) {
 }
 
 var intervalMs = convertToMilliseconds(interval);
-console.log(intervalMs)
 
 setInterval(function() {
   var divButtons = document.querySelectorAll(".action_label");
