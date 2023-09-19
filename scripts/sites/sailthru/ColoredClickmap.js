d71e01363b7ecd64f3d74d8e6f6016f7f2534b9f
Colin-Whelan <38737123+Colin-Whelan@users.noreply.github.com>
2023-09-19 15:47:20 -0400

    Update ColoredClickmap.js

--- scripts/sites/sailthru/ColoredClickmap.js
+++ scripts/sites/sailthru/ColoredClickmap.js
@@ -9,20 +9,27 @@
 // @require https://code.jquery.com/jquery-3.6.0.min.js
 // ==/UserScript==
 
-(function() {
-    'use strict';
+// Wrap the code in a function
+function updateHeatmap() {
+    // Clear existing unique classes
+    $('[class^="heatmap-"]').removeClass(function(index, className) {
+        return (className.match(/(^|\s)heatmap-\S+/g) || []).join(' ');
+    });
 
     let clickValues = [];
 
+    const isPercentMode = $('.clickmap_select').val() === 'click_percentage';
+
     // Collect all the click numbers first
     $('td.pad span').each(function() {
-    const clicks = parseInt($(this).text(), 10);
+        const text = $(this).text().trim().replace('%','');
+        const isNumber = !isNaN(text) && !isNaN(parseFloat(text));
+        const clicks = isNumber ? parseFloat(text) : null;
+
         if (!isNaN(clicks)) {
             clickValues.push(clicks);
         }
     });
-
-    console.log(clickValues)
 
     // Determine min and max click values
     const minClicks = Math.min(...clickValues);
@@ -35,14 +42,12 @@
 
         if (ratio < 0.25) {
             hue = 0 + (ratio / 0.25) * 30; // Red to Orange
-        } else if (ratio < 0.35) {
-            hue = 30 + ((ratio - 0.25) / 0.25) * 30; // Orange to Yellow
         } else if (ratio < 0.5) {
+            hue = 30 + ((ratio - 0.25) / 0.25) * 30; // Orange to Yellow
+        } else if (ratio < 0.75) {
             hue = 60 + ((ratio - 0.5) / 0.25) * 60; // Yellow to Green
-        } else if (ratio < 0.65) {
-            hue = 120 + ((ratio - 0.5) / 0.25) * 60; // Yellow to Green
         } else {
-            hue = 0; // Green
+            hue = 120; // Green
         }
 
         return `hsl(${hue}, 100%, 50%)`;
@@ -56,46 +61,66 @@
         const $span = $td.find('span');
 
         const clicks = parseInt($span.text(), 10);
-        const color = getHeatmapColor(clicks);
+        if (!isNaN(clicks)) {
+           const color = getHeatmapColor(clicks);
 
-        // Generate a unique class for each color to add as a pseudo-element for the fade effect
-        const uniqueClass = 'heatmap-' + clicks;
+          // Generate a unique class for each color to add as a pseudo-element for the fade effect
+          const uniqueClass = 'heatmap-' + clicks;
+          // console.log('uniqueClass: ', uniqueClass, clicks)
 
-        // Inject styles for the pseudo-element
-        $('head').append(`
-            <style>
-                .${uniqueClass}::before {
-                    content: "";
-                    position: absolute;
-                    width: 400%;
-                    height: 400%;
-                    top: -150%;
-                    left: -150%;
-                    right: -150%;
-                    bottom: -150%;
-                    background: radial-gradient(circle, ${color} 0%, transparent 100%);
-                    z-index: -1;
-                }
-            </style>
-        `);
+          // Inject styles for the pseudo-element
+          $('head').append(`
+              <style>
+                  .${uniqueClass}::before {
+                      content: "";
+                      position: absolute;
+                      width: 400%;
+                      height: 400%;
+                      top: -150%;
+                      left: -150%;
+                      right: -150%;
+                      bottom: -150%;
+                      background: radial-gradient(circle, ${color} 0%, transparent 100%);
+                      z-index: -1;
+                  }
+              </style>
+          `);
 
-        // Add the unique class to the span and adjust its position
-        $span.addClass(uniqueClass);
+          // Add the unique class to the span and adjust its position
+          $span.addClass(uniqueClass);
 
-        // Move the number down a bit to align with the image a bit better
-        const thisImg = $td.find('img');
-        if(thisImg.get(0)) {
-          console.log(thisImg.get(0).height)
+          // Move the number down a bit to align with the image a bit better
+          const thisImg = $td.find('img');
+          if(thisImg.get(0)) {
+            const marginTop = thisImg.get(0).height * .13
 
-          const marginTop = thisImg.get(0).height * .13
+            $span.css({
+                'margin-top': `${marginTop}px`,
+                'font-weight': 'bold',
+                'font-size': '16px',
+                'color': 'white',
+                'text-shadow': '0px 0px 3px black, 0px 0px 3px black, 0px 0px 3px black, 0px 0px 3px black'
+            })
+          } else {
 
-          $span.css({
-              'margin-top': `${marginTop}px`,
-              'font-weight': 'bold',
-              'font-size': '16px',
-              'color': 'white',
-          })
+            $span.css({
+                'font-weight': 'bold',
+                'font-size': '16px',
+                'color': 'white',
+                'text-shadow': '0px 0px 3px black, 0px 0px 3px black, 0px 0px 3px black, 0px 0px 3px black'
+            })
+
+          }
         }
 
+
     });
-})();
+}
+
+// Initially call the function to set up the heatmap
+updateHeatmap();
+
+// Add a change event listener to the select dropdown
+$('.clickmap_select').change(function() {
+    updateHeatmap();
+});
