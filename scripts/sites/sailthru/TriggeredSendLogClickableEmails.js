2bc6fa2fc0cff7a2f9a0230914909173c6b8e394
Colin-Whelan <38737123+Colin-Whelan@users.noreply.github.com>
2023-10-04 13:23:38 -0400

    Update TriggeredSendLogClickableEmails.js

--- scripts/sites/sailthru/TriggeredSendLogClickableEmails.js
+++ scripts/sites/sailthru/TriggeredSendLogClickableEmails.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        Triggered Send Log - Clickable Emails
 // @namespace   Violentmonkey Scripts
-// @match       https://my.sailthru.com/reports/transactional_log
+// @match       https://my.sailthru.com/reports/transactional_log*
 // @grant       none
 // @version     1.0
 // @author      -
-// @description 2023-09-21, 2:45:27 p.m.
+// @description Makes email addresses clickable in the Triggered Send Log which lead to the User Profile.
 // ==/UserScript==
 
 // Function to convert emails to links
