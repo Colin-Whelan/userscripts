84930cc358f19223df7c6b5ba78e4511e1277563
Colin-Whelan <38737123+Colin-Whelan@users.noreply.github.com>
2023-10-07 01:36:19 -0400

    Update ImprovedHtmlEditor.js

--- scripts/sites/sailthru/ImprovedHtmlEditor.js
+++ scripts/sites/sailthru/ImprovedHtmlEditor.js
@@ -1,13 +1,53 @@
 // ==UserScript==
-// @name        HTML Composer - Improved Ace Editor for Zephyr and HTML
+// @name        HTML Editor - Better Ace IDE
 // @namespace   Violentmonkey Scripts
 // @match       https://my.sailthru.com/template/*
 // @grant       none
 // @run-at      document-end
-// @version     1.0
+// @version     1.1
 // @author      Colin Whelan
 // @description Improved HTML Editor (Ace Editor) by updating the config settings. Update as needed to suit your preferences.
+// Add the following:
+// - custom IDE themes
+// - The better 'find' window by default. Supports regex
+// - Autosize editor
+// - Default options
 // ==/UserScript==
+
+// Default Options
+const fontSize = 13
+const minLines = 16
+const tabSize = 2
+const dragDelay = 0
+const fontFamily = "Fira Code"
+const showPrintMargin = false
+
+function executeInPageContext(fn) {
+    const script = document.createElement('script');
+    script.textContent = '(' + fn.toString() + ')();';
+    (document.head || document.documentElement).appendChild(script);
+    script.parentNode.removeChild(script);
+}
+
+function enableCommands(editor) {
+    var defaultCommands = ace.require("ace/commands/default_commands").commands;
+
+    var commandNames = [
+        "find", "togglecomment", "showSettingsMenu",
+        "gotoline", "findnext", "findprevious", "indent",
+        "outdent", "fold", "unfold", "foldall",
+        "unfoldall", "toggleBlockComment", "transposeletters",
+        "jumptomatching", "autoindent"
+    ];
+
+    commandNames.forEach(function(name) {
+        var cmd = defaultCommands.filter(function(c) { return c.name == name; })[0];
+        if (cmd) {
+            console.log(cmd)
+            editor.commands.addCommand(cmd);
+        }
+    });
+}
 
 function improveEditor() {
     if (window.ace) {
@@ -17,6 +57,7 @@ function improveEditor() {
 
         // Set theme to Monokai
         editor.setTheme("ace/theme/monokai");
+
         // Other options:
         // 'ambiance', 'chaos', 'clouds', 'clouds_midnight', 'cobalt',
         // 'dawn', 'dreamweaver', 'eclipse',
@@ -26,14 +67,10 @@ function improveEditor() {
         // 'tomorrow_night', 'tomorrow_night_blue', 'tomorrow_night_bright',
         // 'tomorrow_night_eighties', 'twilight', 'vibrant_ink', 'xcode'
 
-        // Make Editor Resizable
         const editorDiv = document.getElementById("ace-editor");
 
         if (editorDiv) {
-            // editorDiv.style.resize = "both";
-            // editorDiv.style.overflow = "auto";
-
-            const lineHeight = 15.7667; // Height of each line in px
+            const lineHeight = fontSize * 1.3; // Height of each line in px
 
             const setEditorOptions = () => {
                 const windowHeight = window.innerHeight;
@@ -44,33 +81,44 @@ function improveEditor() {
                 // Update other options
                 editor.setOptions({
                     maxLines: lines,  // Default: 16
-                    minLines: 16,  // Default: 16
-                    showPrintMargin: false,  // Default: true
+                    minLines: minLines,  // Default: 16
+                    showPrintMargin: showPrintMargin,  // Default: true
                     navigateWithinSoftTabs: true,  // Default: null
-                    tabSize: 2, // Default: 4
+                    tabSize: tabSize, // Default: 4
                     wrap: true, // Default: "free"
                     wrapMethod: "code", // Default: null
                     indentedSoftWrap: true, // Default: null
                     animatedScroll: true, // Default: false
-                    scrollPastEnd: 8, // Default: 0 - May not work
-                    dragDelay: 0, // Default: 150
+                    dragDelay: dragDelay, // Default: 150
+                    enableMultiSelect: false,
+                    enableAutoIndent: false,
+                    selectionStyle: 'line',
 
-                    // WIP - Not working
+                    // Gutter
+                    firstLineNumber: 1,
+                    fixedWidthGutter: true,
+
+                    //Font
+                    fontFamily: fontFamily,
+                    fontSize: fontSize,
+
+                    // WIP/Not working
+                    scrollPastEnd: 1, // Default: 0 - May not work
                     enableBasicAutocompletion: true,
                     enableSnippets: true,
+                    enableCodeLens: true,
+                    enableEmmet: true,
+                    enableInlineAutocompletion: true,
                     enableLiveAutocompletion: true
                 });
 
-              // console.log(ace.config)
-              // console.log(editor.getOptions())
+                executeInPageContext(enableCommands(editor));
             };
 
             // Initial set and update on window resize
             setEditorOptions();
             window.addEventListener("resize", setEditorOptions);
         }
-
-        console.log("Ace Editor improved with Zephyr support.");
     }
 }
 
