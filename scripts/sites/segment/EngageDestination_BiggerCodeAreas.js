41ace75aaf889aeb49ecdb88027c526d91f18d2a
Colin-Whelan <38737123+Colin-Whelan@users.noreply.github.com>
2023-11-03 12:01:20 -0400

    Update EngageDestination_BiggerCodeAreas.js

--- scripts/sites/segment/EngageDestination_BiggerCodeAreas.js
+++ scripts/sites/segment/EngageDestination_BiggerCodeAreas.js
@@ -5,21 +5,48 @@
 // @match       https://app.segment.com/*/engage/spaces/*/audiences/*
 // @match       https://app.segment.com/*
 // @grant       GM_addStyle
-// @version     1.2
+// @version     1.3
 // @author      Colin Whelan
-// @description Makes text areas for JSON bigger and easier to work with. Makes 'New Audience' a bit better. Makes 'And who/And who not' more visible. Audience window takes full space. Option for custom tooltips in audience conditions.
+// @description Adds these improvements:
+// - Makes text areas for JSON bigger and easier to work with.
+// - Makes 'New Audience' a bit better.
+// - Makes 'And who/And who not' more visible.
+// - Audience window takes full space.
+// - Option for custom tooltips in audience conditions.
+// - Adds custom quick links to the sidebar
 // ==/UserScript==
 
 
 const addConditionTooltips = false
 
 const tips = [
-  `event tip`, // event
-  `audience tip`, // audience
-  `computedTrait tip`, // computedTrait
-  `sql tip`, // sql
-  `customTrait tip` // customTrait
+  `API Event+Payload`, // event
+  `Part of a List`, // audience
+  `Campaign interactions or test groups`, // computedTrait
+  `Customer flags`, // sql
+  `Customer details(PII)` // customTrait
 ]
+
+const addCustomQuicklinks = false
+
+const quicklinksJson = `[
+{
+    "textToShow": "Custom Link 1",
+    "url": "engage/spaces/instanceName/audiences/new"
+},
+{
+    "textToShow": "Custom Link 2",
+    "url": "unify/spaces/instanceName/explorer"
+},
+{
+    "textToShow": "Destinations",
+    "url": "destinations"
+},
+{
+    "textToShow": "Libraries",
+    "url": "protocols/libraries"
+}
+]`;
 
 const urlPattern = /^https:\/\/app\.segment\.com\/[^\/]+\/engage\/spaces\/[^\/]+\/audiences\/new$/;
 const currentUrl = window.location.href;
@@ -121,9 +148,8 @@ function addTooltips() {
     for (let mutation of mutationsList) {
       if (mutation.addedNodes.length) {
         mutation.addedNodes.forEach((node) => {
-          if (node.role == 'dialog' && node.attributes['data-state'] && node.attributes['data-state'].value) {
+          if (node.role == 'dialog' && node.attributes['data-state'] && node.attributes['data-state'].value && node.textContent != 'AudienceFolder') {
             tooltipObserver.disconnect()
-            console.log('adding tooltips')
             node.style.width = '400px'
 
             // Set the parent element to a flex container to center its children
@@ -136,8 +162,6 @@ function addTooltips() {
             let childCount = 0
 
             for(let child of node.firstChild.children) {
-
-              console.log(child)
               child.classList.add('custom-tooltip')
               child.classList.add('📦otln_iu2jf4_1849h0v', '📦bg_2u3whs_1849h0v', '📦bs_iarjze_1849h0v', 'ub-box-szg_border-box', '📦flx_1', '📦dspl_flex', '📦algn-itms_center', '📦flx-srnk_0', '📦ovflw-x_hidden', '📦ovflw-y_hidden', '📦pl_12px', '📦pr_12px');
               const tooltipDiv = document.createElement('div');
@@ -154,7 +178,7 @@ function addTooltips() {
           }
           else {
             if(node.attributes['data-state'] && node.attributes['data-state'].value){
-              console.log(node.role, node.attributes['data-state'].value)
+              // console.log(node.role, node.attributes['data-state'].value)
             }
 
           }
@@ -170,5 +194,77 @@ function addTooltips() {
 // Call the function to start adding tooltips
 if(addConditionTooltips){
   addTooltips();
+}
+
+function addQuicklinks(){
+  // Parse the JSON structure
+  const quicklinks = JSON.parse(quicklinksJson);
+
+  // Target div where existing links are placed
+  const targetDiv = document.querySelector('.fs-unmask.css-r8gsu9');
+
+  // Function to create a link element
+  function createLinkElement(text, url, i) {
+      let workspace = document.querySelector('strong.css-1wa6wcq').textContent
+      workspace = workspace.replaceAll(" ", "-").toLowerCase()
+      const div = document.createElement('div');
+      div.className = 'css-1c0qan';
+      div.id = `quicklink_${i}`;
+      div.innerHTML = `<a class="📦pl_16px 📦pr_16px 📦dspl_flex 📦algn-itms_center 📦flx-drct_row 📦color_white 📦pst_relative 📦h_40px 📦crsr_pointer 📦mb_8px 📦mt_8px ub-box-szg_border-box" href="/${workspace}/${url}">
+                          <span class="📦fnt-sze_14px 📦f-wght_600 📦ln-ht_20px 📦ltr-spc_-0-05px 📦fnt-fam_b77syt 📦color_c1c4d6 📦ml_16px ub-box-szg_border-box">${text}</span>
+                       </a>`;
+      return div;
+  }
+
+  // Configuration of the observer:
+  const config = { childList: true, subtree: true };
+
+  // Callback function to execute when mutations are observed
+  const callback = function(mutationsList, observer) {
+      for(const mutation of mutationsList) {
+          if (mutation.type === 'childList') {
+              const targetDiv = document.querySelector('.fs-unmask.css-r8gsu9');
+              if (targetDiv && targetDiv.children[0]) {
+                  // Stop observing
+                  observer.disconnect();
+
+                  // create the header and append it to the target div
+                  if(!document.getElementById('quicklinksHeader')){
+                    const header = document.createElement('div');
+                    header.id = 'quicklinksHeader';
+                    header.style.borderTop = '1px solid #ccc'; // Change the color as needed
+                    // header.style.fontWeight = 'bold';
+                    // header.style.fontSize = '18px';
+                    header.style.textAlign = 'left';
+                    header.style.marginTop = '10px'; // Adds space above the border
+                    header.style.paddingTop = '10px';
+                    header.classList.add('📦pl_16px','📦pl_16px','📦pr_16px','📦dspl_flex','📦algn-itms_center','📦flx-drct_row','📦color_white','📦pst_relative','📦h_40px','📦mb_8px','📦mt_8px','ub-box-szg_border-box')
+                    header.innerHTML = '<span class="📦f-wght_600 📦ltr-spc_-0-05px 📦fnt-fam_b77syt 📦color_c1c4d6 📦ml_16px ub-box-szg_border-box" style=""><span style="font-size: 18px;line-height: 32px;">Quick Links</span></span>'
+
+                    // Append the header to the target div
+                    targetDiv.children[0].appendChild(header); // This will add the header at the top of the target div
+                  }
+
+                  // Add each quick link to the target div
+                  quicklinks.forEach((link, index) => {
+                      const linkElement = createLinkElement(link.textToShow, link.url, index);
+                      if(document.querySelector(`#quicklink_${index}`)) return
+                      targetDiv.children[0].appendChild(linkElement);
+                  });
+              }
+          }
+      }
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
+
+  // Start observing the document body for configured mutations
+  observer.observe(document.body, config);
+}
+
+// Call the function to start adding quicklinks
+if(addCustomQuicklinks){
+  addQuicklinks();
 }
 
